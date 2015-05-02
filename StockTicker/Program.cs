c591c309df3712cd5b79babccee7891c03fcad32
Andrew Harcourt <andrewh@uglybugger.org>
2015-05-02 12:18:24 +1000

    Created first console app. We loop forever and print to the console.

---
+++ StockTicker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StockTicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stockPrice = 0.10m;
+
+            while (!Console.KeyAvailable)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+                Console.WriteLine("Stock price for READIFY is {0}", stockPrice);
+                // publish "StockPriceChangedEvent"
+                stockPrice *= 1.01m;
+            }
+        }
+    }
+}
